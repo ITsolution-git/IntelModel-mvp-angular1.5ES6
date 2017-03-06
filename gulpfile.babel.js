8b58552733a9a383e18d433e651eccc71a481963
jinbo chen <jinbo.chen@gmail.com>
2017-03-05 19:44:57 -0800

    try a commit

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -572,7 +572,7 @@ grunt.initConfig({
         },
         heroku: {
             options: {
-                remote: 'heroku',
+                remote: 'https://git.heroku.com/intel-modal.git',
                 branch: 'master'
             }
         },
