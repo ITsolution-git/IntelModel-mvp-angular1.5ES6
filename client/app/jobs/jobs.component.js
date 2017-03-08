159b7465d2adffeb2eb27d32d8cfe9706913be6c
jinbo chen <jinbo.chen@gmail.com>
2017-03-07 22:42:16 -0800

    try a commit

--- client/app/jobs/jobs.component.js
+++ client/app/jobs/jobs.component.js
@@ -8,22 +8,30 @@ import routes from './jobs.routes';
 export class JobsComponent {
 
   /*@ngInject*/
-  constructor() {
+  constructor($location) {
     var data = [
-      { name: "Job1", status: 'In Transit', containers:3, type: 'Import', updated: '3 mins ago' },
-      { name: "Job2", status: 'Delivered',  containers:2, type: 'Export', updated: '3 mins ago' },
-      { name: "Job3", status: 'In Transit', containers:1, type: 'Export', updated: '3 mins ago' },
-      { name: "Job4", status: 'Dispatched', containers:3, type: 'Cross Town', updated: '3 mins ago' },
-      { name: "Job5", status: 'In Transit', containers:4, type: 'Import', updated: '3 mins ago' },
-      { name: "Job6", status: 'NEW', containers:2, type: 'Export', updated: '3 mins ago' },
-      { name: "Job7", status: 'In Transit', containers:1, type: 'Import', updated: '3 mins ago' }
+      { _id:1,name: "Job1", status: 'In Transit', progress: 30, containers:3, type: 'Import', updated: '3 mins ago' },
+      { _id:2, name: "Job2", status: 'Delivered',  progress: 100, containers:2, type: 'Export', updated: '3 mins ago' },
+      { _id:3, name: "Job3", status: 'In Transit', progress: 30, containers:1, type: 'Export', updated: '3 mins ago' },
+      { _id:4, name: "Job4", status: 'Dispatched', progress: 10, containers:3, type: 'Cross Town', updated: '3 mins ago' },
+      { _id:5, name: "Job5", status: 'In Transit', progress: 90, containers:4, type: 'Import', updated: '3 mins ago' },
+      { _id:6, name: "Job6", status: 'NEW', progress: 0, containers:2, type: 'Export', updated: '3 mins ago' },
+      { _id:7, name: "Job7", status: 'In Transit', progress: 50, containers:1, type: 'Import', updated: '3 mins ago' }
     ];
     this.tableParams = new NgTableParams({}, { dataset: data});
+    this.location = $location;
   }
 
-  addImportJob() {
+  displayJob(job) {
+    if (job.type == 'Import') {
+      this.location.path("/jobs/")
+    }else if(job.type=='Export') {
 
+    }else {
+
+    }
   }
+
 }
 
 export default angular.module('intelmodalApp.jobs', [ngRoute])
